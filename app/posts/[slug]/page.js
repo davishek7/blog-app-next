41f56d363e845d2570f540573c754d0cb9b11598
davishek7 <davishek7@gmail.com>
2025-09-13 18:22:33 +0530

    added not found on blog details page

--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { API_URL } from "@/constants/api.constant";
 import PostContent from "@/components/PostContent";
 
@@ -5,6 +6,10 @@ export default async function PostDetail({ params }) {
   const { slug } = await params;
   const response = await fetch(`${API_URL}/blog/${slug}`);
   const responseData = await response.json();
+
+  if (responseData.status !== 200){
+    notFound()
+  }
   const post = responseData.data;
 
   return <PostContent post={post} />;
